cfecfe1979af751fee2be034466fc0815e730eed
sharma-arjun <arjuns2@andrew.cmu.edu>
2018-04-28 19:11:37 -0400

    renamed to RL

---
+++ RL/core/common.py
@@ -0,0 +1,24 @@
+
+
+def estimate_advantages(rewards, masks, values, gamma, tau, use_gpu):
+    if use_gpu:
+        rewards, masks, values = rewards.cpu(), masks.cpu(), values.cpu()
+    tensor_type = type(rewards)
+    deltas = tensor_type(rewards.size(0), 1)
+    advantages = tensor_type(rewards.size(0), 1)
+
+    prev_value = 0
+    prev_advantage = 0
+    for i in reversed(range(rewards.size(0))):
+        deltas[i] = rewards[i] + gamma * prev_value * masks[i] - values[i]
+        advantages[i] = deltas[i] + gamma * tau * prev_advantage * masks[i]
+
+        prev_value = values[i, 0]
+        prev_advantage = advantages[i, 0]
+
+    returns = values + advantages
+    advantages = (advantages - advantages.mean()) / advantages.std()
+
+    if use_gpu:
+        advantages, returns = advantages.cuda(), returns.cuda()
+    return advantages, returns
---
+++ RL/core/ppo.py
@@ -0,0 +1,45 @@
+import torch
+#import torch._utils
+#try:
+#    torch._utils._rebuild_tensor_v2
+#except AttributeError:
+#    def _rebuild_tensor_v2(storage, storage_offset, size, stride, requires_grad, backward_hooks):
+#        dummy_tensor = torch._utils._rebuild_tensor(storage, storage_offset, size, stride)
+#        dummy_tensor.requires_grad = requires_grad
+#        dummy_tensor._backward_hooks = backward_hooks
+#        return dummy_tensor
+#    torch._utils._rebuild_tensor_v2 = _rebuild_tensor_v2
+
+from torch.autograd import Variable
+
+
+def ppo_step(policy_net, value_net, optimizer_policy, optimizer_value, optim_value_iternum, states, actions,
+             returns, advantages, fixed_log_probs, lr_mult, lr, clip_epsilon, l2_reg):
+
+    optimizer_policy.lr = lr * lr_mult
+    optimizer_value.lr = lr * lr_mult
+    clip_epsilon = clip_epsilon * lr_mult
+
+    """update critic"""
+    values_target = Variable(returns)
+    for _ in range(optim_value_iternum):
+        values_pred = value_net(Variable(states))
+        value_loss = (values_pred - values_target).pow(2).mean()
+        # weight decay
+        for param in value_net.parameters():
+            value_loss += param.pow(2).sum() * l2_reg
+        optimizer_value.zero_grad()
+        value_loss.backward()
+        optimizer_value.step()
+
+    """update policy"""
+    advantages_var = Variable(advantages)
+    log_probs = policy_net.get_log_prob(Variable(states), Variable(actions))
+    ratio = torch.exp(log_probs - Variable(fixed_log_probs))
+    surr1 = ratio * advantages_var
+    surr2 = torch.clamp(ratio, 1.0 - clip_epsilon, 1.0 + clip_epsilon) * advantages_var
+    policy_surr = -torch.min(surr1, surr2).mean()
+    optimizer_policy.zero_grad()
+    policy_surr.backward()
+    torch.nn.utils.clip_grad_norm(policy_net.parameters(), 40)
+    optimizer_policy.step()
