53e918e46202a6cb0935a77e88d72ff974be1878
Mohit <mohit.sharma0690@gmail.com>
2018-09-18 16:56:39 -0400

    Update path to run gail on hopper.

--- IL/run_continuous.sh
+++ IL/run_continuous.sh
@@ -23,14 +23,14 @@ args=(
   --env-name Hopper-v2
 
   --posterior_learning_rate 0.0
-  --lambda_posterior 0.001
+  --lambda_posterior 0.1
   --use_value_net
   --optim_batch_size 256
 
   # --vae_checkpoint_path ./results/walker/vae/batch_64_context_4_no_time/results/checkpoint/cp_1000.pth
   --vae_checkpoint_path ./results/hopper/discrete_vae/batch_64_context_4_no_time/results/checkpoint/cp_640.pth
 
-  --results_dir ./results/hopper/gail_fixed/rebuttal_batch_4096_cp_640_ep_5000_num_expert_100_policy_log_std_clamped_use_posterior_reward_lambda_posterior_0.001_time_Sept_17_11_59_PM
+  --results_dir ./results/hopper/gail_fixed_fast_save_25/rebuttal_batch_4096_cp_640_ep_5000_num_expert_100_policy_log_std_clamped_use_posterior_reward_lambda_posterior_0.1_time_Sept_17_11_59_PM
 
   # --checkpoint_path ./results/hopper/gail/rebuttal_batch_4096_cp_640_ep_5000_num_expert_100_policy_log_std_clamped_use_posterior_reward_lambda_posterior_0.01_time_Aug_6_00_05_AM/checkpoint/cp_1000.pth
 
