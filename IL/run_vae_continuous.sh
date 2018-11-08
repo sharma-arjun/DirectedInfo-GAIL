d753db33ce45907c202ac616b47c14226cff0e43
Mohit <mohit.sharma0690@gmail.com>
2018-11-07 19:10:09 -0500

    Update vae run script for fetch pick and place.

--- IL/run_vae_continuous.sh
+++ IL/run_vae_continuous.sh
@@ -1,33 +1,39 @@
 BATCH_SIZE=64
 
 args=(
-  --expert-path ./h5_trajs/mujoco_trajs/normal_hopper/rebuttal_hopper_100/
+  --expert-path ./h5_trajs/fetch_pick_and_place_trajs/fetch_100
+  # --expert-path ./h5_trajs/mujoco_trajs/normal_hopper/rebuttal_hopper_100/
 
   --batch-size 64
-  --num-epochs 2000
+  --num-epochs 1000
   --use_rnn_goal 0
   --use_goal_in_policy 0
   --use_separate_goal_policy 1
   --use_discrete_vae
-  --cosine_similarity_loss_weight 1.0
+  --cosine_similarity_loss_weight 0.0
 
-  --vae_state_size 11
-  --vae_action_size 3
+  --vae_state_size 25 
+  --vae_action_size 4
   --vae_goal_size 1
-  --vae_history_size 1
-  --vae_context_size 4
+  --vae_history_size 5
+  --vae_context_size 2
 
   --no-use_state_features
   --continuous_action
-  --env-name Hopper-v2
+  --env-name FetchPickAndPlace-v1
   --env-type mujoco
-  --episode_len 1000
-  --run_mode train
+  --episode_len 50
+  --run_mode train 
 
   --checkpoint_every_epoch 20
 
-  --results_dir ./results/hopper/discrete_vae/batch_64_context_4_no_time_try_2_cos_similarity/
+  --results_dir ./results/fetch_pick_and_place/discrete_vae/batch_64_context_2_no_time_cos_similarity_0.0_decay_33e-4/
 
+ # --checkpoint_path ./results/hopper/discrete_vae/batch_64_context_6_no_time_cos_similarity_1.0_decay_33e-4/checkpoint/cp_540.pth
+
+  # --checkpoint_path ./results/hopper/discrete_vae/batch_64_context_6_no_time_cos_similarity_0.0_decay_33e-4/checkpoint/cp_700.pth
+
+  # --checkpoint_path ./results/hopper/discrete_vae/batch_64_context_6_no_time_try_2_cos_similarity_0.1/checkpoint/cp_1000.pth
   # --checkpoint_path ./results/hopper/discrete_vae/batch_64_context_8_no_time_try_2/checkpoint/cp_2000.pth
 
   --cuda
@@ -35,7 +41,7 @@ args=(
 
 echo "${args[@]}"
 
-python -m pdb vae.py "${args[@]}"
+CUDA_VISIBLE_DEVICES=1 python -m pdb vae.py "${args[@]}"
 
 # Walker
 #python -m pdb vae.py \
